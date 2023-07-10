4a07fbf4518de3b98159eb08c9f410d98ea07183
Elio Briceño <eliobricenov@gmail.com>
2023-07-09 22:12:53 -0400

    feat: menu mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 import { gotham, robotoMono } from "@/fonts";
 import { MechanismCategory } from "@/types/mechanism-category";
-import Link from "next/link";
-import Image from "next/image";
-import Typography from "@/components/ui/typography";
 import Providers from "@/components/providers";
 import { cn } from "@/utils/cn";
+import Navbar from "@/components/navbar";
 
 // this is needed because otherwise the tailwind JIT compiler will not be able to find the dynamic classes
 // the type is to ensure every category is covered
@@ -19,27 +17,6 @@ const backgroundVariants: Record<MechanismCategory, string> = {
   governance: "bg-brown",
   identity: "bg-pink",
 };
-
-function Navbar() {
-  return (
-    <nav className="py-8 px-2 border-b border-divider flex justify-between items-center w-full">
-      <Link href="/">
-        <Image src="/logo.svg" alt="logo" width={208} height={34} />
-      </Link>
-      <div className="hidden gap-10 md:flex">
-        <Typography variant="nav-link" asChild>
-          <Link href="/about">About</Link>
-        </Typography>
-        <Typography variant="nav-link" asChild>
-          <Link href="/library">Library</Link>
-        </Typography>
-        <Typography variant="nav-link" asChild>
-          <Link href="/get-involved">Get Involved</Link>
-        </Typography>
-      </div>
-    </nav>
-  );
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
---
+++ components/navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Typography from "@/components/ui/typography";
+import { useState } from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { Menu } from "lucide-react";
+import { Transition } from "@headlessui/react";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav className="py-8 px-2 border-b border-divider flex justify-between items-center w-full">
+      <Link href="/">
+        <Image src="/logo.svg" alt="logo" width={208} height={34} />
+      </Link>
+
+      <div className="hidden gap-10 md:flex">
+        <Typography variant="nav-link" asChild>
+          <Link href="/about">About</Link>
+        </Typography>
+        <Typography variant="nav-link" asChild>
+          <Link href="/library">Library</Link>
+        </Typography>
+        <Typography variant="nav-link" asChild>
+          <Link href="/get-involved">Get Involved</Link>
+        </Typography>
+      </div>
+
+      <div className="md:hidden">
+        <Dialog.Root open={open} onOpenChange={setOpen}>
+          <Dialog.Trigger className="button">
+            <Menu className="text-gray-600" />
+          </Dialog.Trigger>
+          <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50 z-40" />
+          <Transition
+            as={Dialog.Content}
+            aria-label="Menu"
+            className="fixed right-0 top-0 bg-gray-200 p-6 space-y-4 shadow-xl h-screen z-50 flex flex-col transition-all transform translate-x-full"
+            show={open}
+            enter="transition-all transform ease-out duration-300"
+            enterFrom="opacity-0 translate-x-full"
+            enterTo="opacity-100 translate-x-0"
+            leave="transition-all transform ease-in duration-300"
+            leaveFrom="opacity-100 translate-x-0"
+            leaveTo="opacity-0 translate-x-full"
+          >
+            <Typography variant="nav-link" asChild>
+              <Link href="/about" onClick={() => setOpen(false)}>
+                About
+              </Link>
+            </Typography>
+            <Typography variant="nav-link" asChild>
+              <Link href="/library" onClick={() => setOpen(false)}>
+                Library
+              </Link>
+            </Typography>
+            <Typography variant="nav-link" asChild>
+              <Link href="/get-involved">Get Involved</Link>
+            </Typography>
+          </Transition>
+        </Dialog.Root>
+      </div>
+    </nav>
+  );
+}
