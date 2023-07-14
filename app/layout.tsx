8fe5c4e9af72849993157607a94575035ca92222
Elio Briceño <eliobricenov@gmail.com>
2023-07-13 22:16:20 -0400

    chore: include bottom padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <Providers>
           <Navbar />
-          <main className="flex flex-col flex-1">{children}</main>
+          <main className="flex flex-col flex-1 pb-16">{children}</main>
         </Providers>
       </body>
     </html>
