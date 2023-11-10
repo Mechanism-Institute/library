205ae0a50d109668b11e429c453e1109a0d2be8c
Colin Spence <udappia@gmail.com>
2023-11-09 18:25:58 -0700

    Choir: Show dynamic placeholder before clicking on searchbar

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { FormEvent, useRef } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import clsx from "clsx";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Typography from "./ui/typography";
+import CategoriesTyping from "./categories-typing";
 
 export default function SearchBar() {
   const router = useRouter();
   const searchRef = useRef<HTMLInputElement>(null);
+  const [showDynamicPlaceholder, setShowDynamicPlaceholder] = useState<boolean>(true);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -18,6 +21,12 @@ export default function SearchBar() {
     }
   };
 
+  useEffect(() => {
+    if (!showDynamicPlaceholder) {
+      searchRef.current?.focus();
+    }
+  }, [showDynamicPlaceholder]);
+
   return (
     <>
       <div className="flex flex-col items-center justify-center w-full min-h-[calc(100vh-106px)]">
@@ -26,17 +35,25 @@ export default function SearchBar() {
             <Image src="/hero.svg" alt="" width={1130} height={270} />
           </div>
         </div>
-        <form className="relative w-full max-w-screen-md" onSubmit={(e) => handleSubmit(e)}>
-          <Input
-            ref={searchRef}
-            name="search"
-            type="text"
-            placeholder="Describe your challenge, get solutions."
-            className="pr-[62px] w-full"
-            autoComplete="off"
-          />
-          <AirplaneIcon className={clsx("absolute -translate-y-1/2 top-1/2 right-6 bg-gray-900")} />
-        </form>
+        <div className="w-full max-w-screen-md">
+          {showDynamicPlaceholder ? (
+            <DynamicPlaceholder onClick={() => setShowDynamicPlaceholder(false)} />
+          ) : (
+            <form className="relative w-full" onSubmit={(e) => handleSubmit(e)}>
+              <Input
+                ref={searchRef}
+                name="search"
+                type="text"
+                placeholder="Describe your challenge, get solutions."
+                className="pr-[62px] w-full shadow-lg hover:shadow-medium"
+                autoComplete="off"
+              />
+              <AirplaneIcon
+                className={clsx("absolute -translate-y-1/2 top-1/2 right-6 bg-gray-900")}
+              />
+            </form>
+          )}
+        </div>
       </div>
     </>
   );
@@ -53,5 +70,19 @@ function AirplaneIcon({ className }: { className?: string }) {
     >
       <Image src="/paper-plane.svg" alt="paper-plane" width={14} height={14} />
     </button>
+  );
+}
+
+function DynamicPlaceholder({ onClick }: { onClick: () => void }) {
+  return (
+    <div
+      onClick={onClick}
+      className="flex items-center justify-between w-full px-6 py-[13px] transition duration-300 bg-white shadow-lg cursor-text rounded-2xl group hover:shadow-medium"
+    >
+      <Typography className="leading-full text-[14px] md:text-[16px] text-gray-400 space-x-px">
+        <span>Solve</span> <CategoriesTyping /> <span>with mechanism design</span>
+      </Typography>
+      <AirplaneIcon />
+    </div>
   );
 }
